c41564abeadd012976a618448189e22256eb15be
amrutap3 <amruta.patil2@citiustech.com>
2023-05-11 20:43:51 +0530

    trial 1 test

---
+++ trialCMD/src/trialCMD1/trialCMD2.java
@@ -0,0 +1,14 @@
+package trialCMD1;
+
+public class trialCMD2 {
+
+	public static void main(String[] args) {
+		int i=10;
+		do{
+			System.out.println("I am inside do loop");
+			i--;
+		}while(i>=0);
+
+	}
+
+}
